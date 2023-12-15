23702209e16e9bc9da3d4e45f99572344643b050
JiquanWang99 <951873902@qq.com>
2023-12-15 23:15:28 +0800

    0.0.1

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const { walkdir } = require('./index');
 const path = require('path');
 
-let entry = path.join(process.cwd(), '../');
+let entry = path.join(process.cwd(), 'node_modules');
 walkdir(entry, (p) => {
   console.log('path:', p);
 });
